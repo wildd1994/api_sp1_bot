bc5ecccacb024fe42f0f23bdf4443319e37dfea9
Георгий <dfffgfgf@yandex.ru>
2020-11-09 13:17:29 +0300

    homework is done

--- homework.py
+++ homework.py
@@ -1,20 +1,30 @@
+import logging
 import os
+import time
+
 import requests
 import telegram
-import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    try:
+        homework_name = homework['homework_name']
+    except KeyError as e:
+        logging.exception(f'Key error detected. Error: {e}')
+    except IndexError as e:
+        logging.exception(f'Index error detected. Error: {e}')
+    status = homework['status']
+    if status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,14 +32,18 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=params)
+    except requests.exceptions.RequestException as e:
+        logging.exception(f'Connection error detected. Error: {e}')
+        raise e
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    return bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def main():
@@ -38,6 +52,7 @@ def main():
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
+            print(new_homework)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
